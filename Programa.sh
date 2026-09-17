a28826a705bbab0e80cde4a02b644c830e3e2a71
0183-karla <karla_jimenez2008@hotmail.com>
2026-09-17 12:09:52 -0600

    Update Programa.sh
    Programa para encontrar palabra y en la carpeta

--- Programa.sh
+++ Programa.sh
@@ -2,12 +2,14 @@
 
 #!/bin/bash
 
-echo "Buscando la palabra 'home' en esta carpeta:"
+palabra=$1
+ruta=$2
+
+echo "Buscando la palabra '$palabra' en esta carpeta:"
 echo "-------------------------------------------"
-grep -rli "home" $(pwd)
+grep -rli "$palabra" "$ruta"
 
 #grep se usa para buscar archivos
 # -r busca en la carpeta actual y sus subcarpetas
-# -l mustra el nombre y la ubicacion de los archivos que contienen "home"
+# -l mustra el nombre y la ubicacion de los archivos que contienen la palabra que se indico
 # -i ignora osea acepta resultados sin importar mayusculas o minusculas
-# $(pwd) le indicamos a grep que empiece la búsqueda desde la ruta absoluta de la carpeta actual, para poder devolver rutas absolutas de los archivos buscados
